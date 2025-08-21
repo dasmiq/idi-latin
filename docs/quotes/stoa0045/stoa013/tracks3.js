bec5a0e3a612230646314fe58ba5819ba48bdee2
David Smith <dasmiq@gmail.com>
2025-08-21 00:04:57 -0400

    Adding incrementally.

---
+++ docs/quotes/stoa0045/stoa013/tracks3.js
@@ -0,0 +1,2 @@
+var tracks3 =
+[{"id":"HNJAIW","name":"Eloquentium virorum narrationes de vitis... [1826]","lang":"lat","date":"1826","base":"https://babel.hathitrust.org/cgi/pt?id=hvd.","visible":false,"notecount":1,"notes":[{"start":601,"stop":653,"pre":"","id":618475573696,"url":"hnjaiw&seq=232","sloc":"p. 232"}]}];
