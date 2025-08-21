537b57e47937266ba432e77e287bae1c2cb9a74f
David Smith <dasmiq@gmail.com>
2025-08-21 00:16:20 -0400

    Adding incrementally.

---
+++ docs/quotes/stoa0114a/stoa014/structure.js
@@ -0,0 +1,2 @@
+var structure = [
+{"id": "book", "name": "book", "style": "ac", "notes": [{"id": "1", "name": "1", "start": 0, "stop": 3491}, {"id": "2", "name": "2", "start": 3492, "stop": 5034}]}];
