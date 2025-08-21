9f5769470c24e24b9b6454095546e20934b1d6e3
David Smith <dasmiq@gmail.com>
2025-08-20 22:22:58 -0400

    Adding incrementally.

---
+++ docs/quotes/phi1014/phi004/structure.js
@@ -0,0 +1,2 @@
+var structure = [
+{"id": "book", "name": "book", "style": "ac", "notes": [{"id": "1", "name": "1", "start": 0, "stop": 158}, {"id": "2", "name": "2", "start": 159, "stop": 564}, {"id": "3", "name": "3", "start": 565, "stop": 722}, {"id": "4", "name": "4", "start": 723, "stop": 969}]}];
